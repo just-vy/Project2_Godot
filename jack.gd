c273b565ddde3f96fc6e9ec12d33f6cfab719089
just-vy <i.am.really.really.ugly@gmail.com>
2023-12-29 19:59:36 +0700

    commit-3: Rewrote Character Movements
    just-vy

--- jack.gd
+++ jack.gd
@@ -29,3 +29,21 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+#func _ready():
+#	pass
+#
+#func _physics_process(delta):
+#	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+#		velocity.x = 0
+#	elif Input.is_action_pressed("ui_left"):
+#		velocity.x = -5
+#	elif Input.is_action_pressed("ui_right"):
+#		velocity.x = 5
+#	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+#		velocity.z = 0
+#	elif Input.is_action_pressed("ui_up"):
+#		velocity.z = -5
+#	elif Input.is_action_pressed("ui_down"):
+#		velocity.z = 5
+#	move_and_slide()
